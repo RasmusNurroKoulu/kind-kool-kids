528a4209a3ab575174a731d9048f74622d53babe
Rasmus Nurro <82094563+RasmusNurroKoulu@users.noreply.github.com>
2022-09-14 11:26:38 +0300

    Update server.php

--- server.php
+++ server.php
@@ -49,8 +49,8 @@ if (isset($_POST['reg_user'])) {
   }
 
   // Finally, register user if there are no errors in the form
-  	$query = "INSERT INTO users (etunimi, sukunimi, email, koulutalo, kurssi) 
-  			  VALUES('$etunimi', '$sukunimi', '$email', '$koulutalo', '$kurssi', '$texti')";
+  	$query = "INSERT INTO users (tunnisteetunimi, sukunimi, email, koulutalo, kurssi) 
+  			  VALUES(NULL, '$etunimi', '$sukunimi', '$email', '$koulutalo', '$kurssi', '$texti')";
   	mysqli_query($db, $query);
   	header('location: index.php');
   }
